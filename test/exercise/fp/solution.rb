9b2b55e83ca471e8b944e3e1441dbb799dd5b3ab
Ivan Filimonov <snake73w@gmail.com>
2019-02-07 19:51:52 +0700

    add fp exercise solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,17 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_films = array.select { |film| film['country']&.split(',').to_a.size >= 2 }
+                              .select { |film| film['rating_kinopoisk'].to_f.positive? }
+        sum_of_ratings = filtered_films.inject(0) { |a, e| a + e['rating_kinopoisk'].to_f }
+        sum_of_ratings / filtered_films.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        searched_char = 'и'
+        filtered_films = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
+        filtered_films.inject(0) { |a, e| a + e['name'].count(searched_char) }
       end
     end
   end
