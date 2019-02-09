65cfa8e4e6b55b22cd049621eac810f7cf1b4ef6
Ivan Filimonov <snake73w@gmail.com>
2019-02-09 19:11:37 +0700

    add fp2 exercise solution

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,18 +6,56 @@ module Exercise
 
       # Написать свою функцию my_each
       def my_each
+        iter = lambda do |array|
+          return self if array.size.zero?
+
+          yield array.first
+          new_array = array[1..-1]
+          iter.call(new_array)
+        end
+
+        iter.call(self)
       end
 
       # Написать свою функцию my_map
       def my_map
+        iter = lambda do |acc, array|
+          return acc if array.size.zero?
+
+          acc << yield(array.first)
+          new_array = array[1..-1]
+          iter.call(acc, new_array)
+        end
+
+        iter.call(MyArray.new([]), dup)
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        iter = lambda do |acc, array|
+          return acc if array.size.zero?
+
+          acc << array.first unless array.first.nil?
+          new_array = array[1..-1]
+          iter.call(acc, new_array)
+        end
+
+        iter.call(MyArray.new([]), self)
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(initial = nil)
+        iter = lambda do |acc, array|
+          return acc if array.size.zero?
+
+          acc = yield(acc, array.first)
+          new_array = array[1..-1]
+          iter.call(acc, new_array)
+        end
+
+        initial_array = initial.nil? ? self[1..-1] : self
+        acc = initial || first
+        iter.call(acc, initial_array)
       end
     end
   end
